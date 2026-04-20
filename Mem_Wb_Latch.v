24d5933678bb73f6c6d4b5691df379df98bae622
mariec-j <mcjardiniano@cpp.edu>
2026-04-20 14:10:52 -0700

    Update Mem_Wb_Latch.v
    - Finish writing out i/o's of the Latch

--- Mem_Wb_Latch.v
+++ Mem_Wb_Latch.v
@@ -1,6 +1,21 @@
 `timescale 1ns / 1ps
 
 module Mem_Wb_Latch(
-    
+// - - - - - - Inputs - - - - - -
+    input [1:0]     WB,
+    input           m_ctlout,
+    input           Zero,
+    input           MemWrite,
+    input [31:0]    ALU_Result,
+    input [31:0]    ReadData2_ex_mem,
+    input           clk, rst,
+    input [4:0]     muxOut_5bit,
+// - - - - - - Outputs - - - - - -
+    output reg          Mem_Wb_Latch_RegWrite,
+    output reg          Mem_Wb_Latch_MemtoReg,
+    output reg [31:0]   Mem_Wb_Latch_ReadData,
+    output reg [31:0]   Mem_Wb_Latch_Mem_ALU_Result,
+    output reg [4:0]    Mem_Wb_Latch_MemWriteReg
     );
+    
 endmodule
