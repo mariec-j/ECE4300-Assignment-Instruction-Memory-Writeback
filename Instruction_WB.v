3d69e2c7c700cadf90b05761c3da3a62c11d2213
mariec-j <mcjardiniano@cpp.edu>
2026-04-23 03:38:58 -0700

    Update Instruction_WB.v
    - oopsies don't need the premade brief

--- Instruction_WB.v
+++ Instruction_WB.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/23/2026 03:35:07 AM
-// Design Name: 
-// Module Name: Instr_WB
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module Instr_WB(
     input MemtoReg,
