6f26268e6053ed7535b1db17c0109d580afa60ca
mariec-j <mcjardiniano@cpp.edu>
2026-04-22 22:18:56 -0700

    Update Mem_Wb_Latch.v
    - oopsies
    - accidently added needless signal inputs
    - changed active low rst which matches previous assignments

--- Mem_Wb_Latch.v
+++ Mem_Wb_Latch.v
@@ -3,11 +3,8 @@
 module Mem_Wb_Latch(
 // - - - - - - Inputs - - - - - -
     input [1:0]     WB,
-    input           m_ctlout,
-    input           Zero,
-    input           MemWrite,
     input [31:0]    ALU_Result,
-    input [31:0]    ReadData2_ex_mem,
+    input [31:0]    ReadData,
     input           clk, rst,
     input [4:0]     muxOut_5bit,
 // - - - - - - Outputs - - - - - -
@@ -18,8 +15,8 @@ module Mem_Wb_Latch(
     output reg [4:0]    Mem_Wb_Latch_MemWriteReg
     );
     
-always @(posedge clk or posedge rst)begin
-    if(rst)begin
+always @(posedge clk or negedge rst)begin
+    if(!rst)begin
         Mem_Wb_Latch_RegWrite <= 0;
         Mem_Wb_Latch_MemtoReg <= 0;
         Mem_Wb_Latch_ReadData <= 0;
@@ -28,7 +25,7 @@ always @(posedge clk or posedge rst)begin
     end else begin
         Mem_Wb_Latch_RegWrite <= WB[1];
         Mem_Wb_Latch_MemtoReg <= WB[0];
-        Mem_Wb_Latch_ReadData <= ReadData2_ex_mem;
+        Mem_Wb_Latch_ReadData <= ReadData;
         Mem_Wb_Latch_Mem_ALU_Result <= ALU_Result;
         Mem_Wb_Latch_MemWriteReg <= muxOut_5bit;
     end
